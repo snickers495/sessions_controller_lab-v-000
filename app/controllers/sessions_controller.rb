670c09d10fd99ecda5b42ab6b817faef1cf2df3f
Lisa Lee <snickers495@gmail.com>
2017-12-08 20:27:56 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,11 +4,11 @@ class SessionsController < ApplicationController
   end
 
   def create
-    if params[:name].nil?
+    if params[:name].nil? || params[:name] == ""
       redirect_to :new
     else
       session[:name] = params[:name]
-
+      
   end
 
   def destroy
